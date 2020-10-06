37a12266e1b9814cf769ee1f1121d77bc1da2b18
Emir013 <emir.mulahalilovic@live.com>
2020-10-06 22:14:28 +0200

    vanitygapps

--- config.def.h
+++ config.def.h
@@ -3,10 +3,10 @@
 /* appearance */
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
-static const unsigned int gappih    = 10;       /* horiz inner gap between windows */
-static const unsigned int gappiv    = 10;       /* vert inner gap between windows */
-static const unsigned int gappoh    = 10;       /* horiz outer gap between windows and screen edge */
-static const unsigned int gappov    = 10;       /* vert outer gap between windows and screen edge */
+static const unsigned int gappih    = 17;       /* horiz inner gap between windows */
+static const unsigned int gappiv    = 17;       /* vert inner gap between windows */
+static const unsigned int gappoh    = 17;       /* horiz outer gap between windows and screen edge */
+static const unsigned int gappov    = 17;       /* vert outer gap between windows and screen edge */
 static const int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
