1b4fb551656363be046d2ea4770e89df44e83b03
Emir013 <emir.mulahalilovic@live.com>
2020-10-12 23:11:53 +0200

    config

--- config.def.h
+++ config.def.h
@@ -3,7 +3,7 @@
 /* appearance */
 static const unsigned int borderpx  = 2;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
-static const int showbar            = 0;        /* 0 means no bar */
+static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "NotoMono Nerd Font:size=7:antialias=true:autohint=true" };
 static const char dmenufont[]       = "NotoMono Nerd Font:size=11:antialias=true:autohint=true";
